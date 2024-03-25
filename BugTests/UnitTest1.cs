4f108906688536fe75b2b18d9c8ceba9a6e4f540
Anton A. Shtanyuk <ashtanyuk@gmail.com>
2024-03-25 09:51:40 +0300

    Initial commit

---
+++ BugTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace BugTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var bug = new Bug(Bug.State.Open);
+        bug.Assign();
+        var state = bug.getState();
+        Assert.AreEqual(state, Bug.State.Assigned);
+    }
+}
